780e189a0bfc064ba9dba90933819ea1d0d6f8e9
mliu95 <michael.liu95@gmail.com>
2013-09-16 10:09:45 -0400

    stylesheets

--- db/migrate/20130916135644_create_users.rb
+++ db/migrate/20130916135644_create_users.rb
@@ -1,6 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :name
+      t.string :bio
+      t.string :major
+      t.boolean :officer
+      t.string :position, default: ""
 
       t.timestamps
     end
