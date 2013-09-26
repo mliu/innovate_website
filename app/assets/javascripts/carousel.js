9dbdd7a7ecd559206c3c1b1ed5eed76041257047
mliu95 <michael.liu95@gmail.com>
2013-09-26 12:22:14 -0400

    slider

---
+++ app/assets/javascripts/carousel.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+//   $('.carousel').carousel({
+//     interval: 4000
+//   });
+// });
+
+$(window).load(function() {
+    $('.flexslider').flexslider({
+      animation: "slide",
+      direction: "vertical",
+      pauseOnHover: true,
+      controlNav: true,
+      directionNav: false,
+      prevText: "",
+      nextText: ""
+    });
+});
--- app/assets/javascripts/cover.js
+++
@@ -1,5 +0,0 @@
-// $(document).ready(function(){
-//   $('.cover-module').click(function(event){
-//     window.location = 'localhost:3000/fellow';
-//   })
-// })
