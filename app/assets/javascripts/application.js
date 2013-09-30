13a48bf117dd9b2f427da2ccd5b000a46abf9468
mliu95 <michael.liu95@gmail.com>
2013-09-30 11:02:11 -0400

    heroku jquery fixing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
+//= require jquery.turbolinks
 //= require turbolinks
 //= require_tree .
 //= require twitter/bootstrap
