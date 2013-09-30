33e46d05e1bc4b55423477dc3f357121b4fc582e
mliu95 <michael.liu95@gmail.com>
2013-09-30 11:16:19 -0400

    jquery heroku

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -4,18 +4,14 @@
 //   });
 // });
 
-$(document).ready(function() {
-  if($.turbo.isReady == true){
-    $('.flexslider').flexslider({
-      animation: "slide",
-      direction: "vertical",
-      pauseOnHover: true,
-      controlNav: true,
-      directionNav: false,
-      prevText: "",
-      nextText: "",
-      slideshowSpeed: 2500,
-      initDelay: 0
-    });
-  }
+$('.flexslider').flexslider({
+  animation: "slide",
+  direction: "vertical",
+  pauseOnHover: true,
+  controlNav: true,
+  directionNav: false,
+  prevText: "",
+  nextText: "",
+  slideshowSpeed: 2500,
+  initDelay: 0
 });
