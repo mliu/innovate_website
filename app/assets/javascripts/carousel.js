aba68b8f0468b4eb8f363497bb844c861738c8fc
mliu95 <michael.liu95@gmail.com>
2013-09-30 20:57:39 -0400

    turbolinks WORKAROUND

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -4,15 +4,33 @@
 //   });
 // });
 
-$(document).on('page:load page:change ready', function(){
+// function carousel(){
+//     $('.flexslider').flexslider({
+//       animation: "slide",
+//       direction: "vertical",
+//       pauseOnHover: true,
+//       controlNav: true,
+//       directionNav: false,
+//       prevText: "",
+//       nextText: "",
+//       slideshowSpeed: 2500,
+//       initDelay: 1000
+//     });  
+// }
+
+// $(document).ready carousel()
+// $(document).on "page:load", carousel()
+
+$(document).on('page:load ready', function(){
     $('.flexslider').flexslider({
-      animation: "slide",
+      // animation: "slide",
+      animationSpeed: 1200,
       direction: "vertical",
       pauseOnHover: true,
       controlNav: true,
       directionNav: false,
-      prevText: "",
-      nextText: "",
+      // prevText: "",
+      // nextText: "",
       slideshowSpeed: 2500,
       initDelay: 1000
     });
